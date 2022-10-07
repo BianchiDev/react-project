a8898cf4cc10fd649a9f4c4ed7a48ef0e4d60094
BianchiDev <ifpebianchi@gmail.com>
2022-10-07 09:01:08 -0300

    Adding module the in integrated

---
+++ pages/css/integracao2.jsx
@@ -0,0 +1,13 @@
+import styles from "./integracao2.module.css"
+
+export default function itegracao() {
+  return (
+    <div id={styles.integracao2}>
+      <div className={styles.vermelho}>cor01#</div>
+      <div className={styles.azul}>cor02#</div>
+      <div className={styles.branco}>cor03#</div>
+    </div>
+
+  )
+}
+/* Os imports do (styles.) devem estar entre conchetes */
